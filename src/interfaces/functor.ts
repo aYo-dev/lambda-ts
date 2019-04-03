b9f69e857ce6617d45a4c08fb966a76613d0fc4c
Asen Yovchev <ayovcheff@gmail.com>
2019-04-03 11:34:50 +0300

    create Functor interface and EitherFunctor

---
+++ src/interfaces/functor.ts
@@ -0,0 +1,5 @@
+export interface IFunctor<T> {
+  map: <A>(f: (v: T) => A) => IFunctor<A>;
+  fold: (f: (v: T) => T, g: (v: T) => T) => T;
+  inspect: (v: string) => IFunctor<T>; // Only for test purposes
+}
