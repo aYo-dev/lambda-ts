0ab82b2b7b9462a1c24867e63b010c85abfbba28
Asen Yovchev <ayovcheff@gmail.com>
2019-10-24 12:18:27 +0300

    export the existing monoid, functor and monads

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Monoids
+export { Sum, Product, All, Any, Max, Min } from './src/lib/monoids';
+// Functors
+export { LazyBox } from './src/lib/functors';
+// Monads
+export { Right, Left, Either, TryCatch } from './src/lib/monads';
