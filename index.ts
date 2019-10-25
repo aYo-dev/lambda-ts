ab46a9cc148c58b51a6b67bfeb092339530e99ee
Asen Yovchev <ayovcheff@gmail.com>
2019-10-25 12:07:53 +0300

    remove index.ts from main folder

--- index.ts
+++
@@ -1,6 +0,0 @@
-// Monoids
-export { Sum, Product, All, Any, Max, Min } from './src/lib/monoids';
-// Functors
-export { LazyBox } from './src/lib/functors';
-// Monads
-export { Right, Left, Either, TryCatch } from './src/lib/monads';
