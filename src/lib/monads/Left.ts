df135f1548112d90d09f301adcc7e9963681d77c
Asen Yovchev <ayovcheff@gmail.com>
2019-11-01 15:04:56 +0200

    the flatMap method of Left container should return Left container of null

--- src/lib/monads/Left.ts
+++ src/lib/monads/Left.ts
@@ -3,7 +3,7 @@ import { IMonad } from "../../interfaces/monad";
 const Left = <T>(value: T): IMonad<T> => ({
   of: <A>(v: A) => Left.of(v),
   map: <A>(f: (v: T) => A) => Left.of(null),
-  flatMap: <A>(f: (v: T) => IMonad<A>) => f(value),
+  flatMap: <A>(f: (v: T) => IMonad<A>) => Left.of(null),
   fold: <A>(f: (v: T) => A, g: (v: T) => T) => f(value),
   inspect: (v): IMonad<T> =>  {
     console.log(v, value);
